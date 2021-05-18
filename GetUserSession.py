ed612d91719fc48f7335dd0b9c325eb02a095e22
Vivek Sudani <viveksudani7@gmail.com>
2021-05-18 09:57:16 +0530

    format code as per PEP8 Guidelines

--- GetUserSession.py
+++ GetUserSession.py
@@ -24,49 +24,53 @@ class ZerodhaAccessToken:
 
     def getaccesstoken(self):
         try:
-            login_url = "https://kite.trade/connect/login?v=3&api_key={apiKey}".format(apiKey=self.apiKey)
+            login_url = "https://kite.trade/connect/login?v=3&api_key={apiKey}".format(
+                apiKey=self.apiKey)
 
-            ##change the chrome driver path
+            # change the chrome driver path
             chrome_driver_path = "chrome_driver_path\\your_machine\\chromedriver.exe"
             options = Options()
 
-            ### By enabling below option you can run chrome without UI
-            #options.add_argument('--headless')
+            # By enabling below option you can run chrome without UI
+            # options.add_argument('--headless')
 
+            # chrome driver object
+            driver = webdriver.Chrome(
+                chrome_driver_path, chrome_options=options)
 
-            ## chrome driver object
-            driver = webdriver.Chrome(chrome_driver_path, chrome_options=options)
-
-            ## load the url into chrome
+            # load the url into chrome
             driver.get(login_url)
 
-            ## wait to load the site
+            # wait to load the site
             wait = WebDriverWait(driver, 20)
 
-            ## Find User Id field and set user id
+            # Find User Id field and set user id
             wait.until(EC.presence_of_element_located((By.XPATH, '//input[@type="text"]')))\
                 .send_keys(self.accountUserName)
 
-            ## Find password field and set user password
+            # Find password field and set user password
             wait.until(EC.presence_of_element_located((By.XPATH, '//input[@type="password"]')))\
                 .send_keys(self.accountPassword)
 
-            ## Find submit button and click
+            # Find submit button and click
             wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]')))\
                 .submit()
 
-            ## Find pin field and set  pin value
-            wait.until(EC.presence_of_element_located((By.XPATH, '//input[@type="password"]'))).click()
+            # Find pin field and set  pin value
+            wait.until(EC.presence_of_element_located(
+                (By.XPATH, '//input[@type="password"]'))).click()
             time.sleep(5)
-            driver.find_element_by_xpath('//input[@type="password"]').send_keys(self.securityPin)
+            driver.find_element_by_xpath(
+                '//input[@type="password"]').send_keys(self.securityPin)
 
-            ## Final Submit
-            wait.until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]'))).submit()
+            # Final Submit
+            wait.until(EC.element_to_be_clickable(
+                (By.XPATH, '//button[@type="submit"]'))).submit()
 
-            ## wait for redirection
+            # wait for redirection
             wait.until(EC.url_contains('status=success'))
 
-            ## get the token url after success
+            # get the token url after success
             tokenurl = driver.current_url
             parsed = urlparse.urlparse(tokenurl)
             driver.close()
@@ -75,8 +79,8 @@ class ZerodhaAccessToken:
             print(ex)
 
 
-## Initialise the class object with required parameters
+# Initialise the class object with required parameters
 _ztoken = ZerodhaAccessToken()
 actual_token = _ztoken.getaccesstoken()
-## Final Token valid for the day ===========>
+# Final Token valid for the day ===========>
 print(actual_token)
